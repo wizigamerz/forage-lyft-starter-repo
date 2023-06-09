e7d394d2829db64da4e6e27c435a577cd1850428
wizigamerz <135843921+wizigamerz@users.noreply.github.com>
2023-06-09 14:38:43 +0100

    Add files via upload

---
+++ car.py
@@ -0,0 +1,10 @@
+
+
+
+class Car():
+    def __init__(self, engine, battery):
+        self.engine = engine
+        self.battery = battery
+
+    def needs_service(self):
+        return self.needs_service() or self.needs_service()
---
+++ engine/spindler_battery.py
@@ -0,0 +1,16 @@
+from Battery import Battery
+from datetime import date
+from dateutil.relativedelta import relativedelta
+
+
+class SpindlerBattery(Battery):
+    def __init__(self, current_date, last_service_date):
+        self.current_date = current_date
+        self.last_service_date = last_service_date
+
+    def needs_service(self):
+        date_which_battery_should_be_serviced_by = self.last_service_date + relativedelta(years=2)
+        if date_which_battery_should_be_serviced_by < self.current_date:
+            return True
+        else:
+            return False
